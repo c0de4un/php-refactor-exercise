88054754748b42088cba9cf04c028ae5066e1b31
c0de4un <code4un@yandex.ru>
2023-11-11 21:54:42 +0400

    ref: Add ItemsFactory to simplify creation and avoid strong dependencies

---
+++ src/Factories/ItemsFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose\Factories;
+
+use GildedRose\Models\EItemTypes;
+use GildedRose\Models\IItem;
+use GildedRose\Models\ItemModel;
+use GildedRose\Models\AgedBrie;
+use GildedRose\Models\BackstagePasses;
+use GildedRose\Models\Sulfuras;
+
+final class ItemsFactory
+{
+
+    private static $itemClassesByType = [
+        EItemTypes::AGED_BRIE        => AgedBrie::class,
+        EItemTypes::SULFURAS         => Sulfuras::class,
+        EItemTypes::BACKSTAGE_PASSES => BackstagePasses::class,
+    ];
+
+    private function __construct()
+    {
+    }
+
+    public static function Create(
+        int $type,
+        int $sellIn,
+        int $quality,
+        string $name = ''
+    ): IItem {
+        /** @var string|null $class */
+        $class = self::$itemClassesByType[$type] || null;
+
+        if ($class) {
+            return new $class($sellIn, $quality);
+        }
+
+        return new ItemModel($name, $sellIn, $quality);
+    }
+
+}
